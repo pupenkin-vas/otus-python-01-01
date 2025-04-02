d91b254654d44f1e28d27f36497f228dacfe21cc
Mustafin_A <pupenkinvas@yandex.ru>
2025-04-03 01:10:33 +0300

    ME:STRUCTLOG - 1:0

--- src/app/logging_config.py
+++ src/app/logging_config.py
@@ -1,14 +1,35 @@
 import logging
+import os
+import structlog
 
 
-def configure_logger(logger_name, level=logging.INFO):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(level)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("[%(asctime)s] "
-                                  "%(name)s "
-                                  "%(levelname)s "
-                                  "%(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+def configure_logger(app_name, log_level=logging.INFO, log_file='./app.log'):
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+            structlog.processors.add_log_level,
+            structlog.processors.EventRenamer("msg"),
+            structlog.stdlib.add_logger_name,
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+
+    )
+
+    logger = structlog.get_logger(app_name)
+    logger.setLevel(log_level)
+
+    console_handler = logging.StreamHandler()
+    console_formatter = logging.Formatter("%(message)s")
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    file_handler = logging.FileHandler(log_file)
+    file_formatter = logging.Formatter("%(message)s")
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
     return logger
